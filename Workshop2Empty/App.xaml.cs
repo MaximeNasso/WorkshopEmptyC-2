050781cbaffe11760fb95352bada931bf3e635ed
Maxime Nasso <47819961+MaximeNasso@users.noreply.github.com>
2020-05-25 22:35:04 +0200

    Workshop2

---
+++ Workshop2Empty/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Workshop2Empty
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //Fonction pour générer une blague par l'API
+        public static string generateJoke(string category)
+        {
+            return null;
+        }
+    }
+}
